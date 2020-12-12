17a102177102d7c5d10e55d31dc255b886967078
good-ho <whdxo100@gmail.com>
2020-12-13 01:02:00 +0900

    Creating Account

--- src/routers/Auth.js
+++ src/routers/Auth.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import { authService } from "fbase";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
   const onChange = (event) => {
     const {
       target: { name, value },
@@ -13,8 +15,24 @@ const Auth = () => {
       setPassword(value);
     }
   };
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
+    let data;
+    try {
+      if (newAccount) {
+        //create Account
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        //login
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <div>
@@ -35,7 +53,7 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value="Login" />
+        <input type="submit" value={newAccount ? "Create Account" : "Login"} />
       </form>
       <div>
         <button>Continue with google</button>

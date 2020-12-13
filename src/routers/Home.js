822a1959330138a7c89b1224e1216980841352ba
good-ho <whdxo100@gmail.com>
2020-12-13 11:33:44 +0900

    home form and db setup

--- src/routers/Home.js
+++ src/routers/Home.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [putwitter, setPutwitter] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setPutwitter(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="What's on your mind?"
+          value={putwitter}
+          onChange={onChange}
+          maxLength={120}
+        />
+        <input type="submit" value="puTwitter" />
+      </form>
+    </div>
+  );
+};
 
 export default Home;

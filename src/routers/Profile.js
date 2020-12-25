1e2b9f1b174cdcdecb53b1339caaec126f409ae3
good-ho <whdxo100@gmail.com>
2020-12-25 20:10:56 +0900

    update profile

--- src/routers/Profile.js
+++ src/routers/Profile.js
@@ -1,15 +1,59 @@
-import React from "react";
-import { authService } from "fbase";
+import React, { useEffect, useState } from "react";
+import { authService, dbService } from "fbase";
 import { useHistory } from "react-router-dom";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
+  const [profileValue, setProfileValue] = useState(userObj.displayName);
   const history = useHistory();
+
   const onLogoutClick = () => {
     authService.signOut();
     history.push("/");
   };
+
+  const getMyPutwitter = async () => {
+    const putwitter = await dbService
+      .collection("putwitter")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt")
+      .get();
+
+    // console.log(putwitter.docs.map((doc) => doc.data()));
+  };
+
+  useEffect(() => {
+    getMyPutwitter();
+  });
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+
+    setProfileValue(value);
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    if (userObj.displayName !== profileValue) {
+      await userObj.updateProfile({
+        displayName: profileValue,
+      });
+    }
+  };
+
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Display name"
+          onChange={onChange}
+          value={profileValue}
+        />
+        <input type="submit" value="Update profile" />
+      </form>
       <button onClick={onLogoutClick}>Log out</button>
     </>
   );
